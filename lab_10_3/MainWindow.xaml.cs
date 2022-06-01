6e0559725b36258ceddf1454e49352c3e5a79c23
su <rakdav@inbox.ru>
2022-06-01 18:07:54 +0200

    1_1

--- lab_10_3/MainWindow.xaml.cs
+++ lab_10_3/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace lab_10_3
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
